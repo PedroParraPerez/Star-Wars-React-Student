c2ffb79d58d58249e8610da4359c14feef075b58
PedroParraPerez <pedroparra06091994@gmail.com>
2022-06-18 12:07:03 +0000

    bug fixeed

--- src/js/views/cardPlanet.js
+++ src/js/views/cardPlanet.js
@@ -4,26 +4,33 @@ import "../../styles/home.css";
 import { Context } from "../store/appContext";
 
 
-export const CardPlanet = () =>{ 
-    const {store}=useContext(Context);
+export const CardPlanet = ({nombre, theid}) =>{ 
+    const {actions}=useContext(Context);
     const [heart,setHeart] = useState(<i className="fa-regular fa-heart"></i>)
     const [color, setColor] = useState("btn btn-outline-warning");
-
-    function addFavorite(){
-        setHeart(<i class="fa-solid fa-heart"></i>);
-        if (color==="btn btn-outline-warning") setColor("btn btn-warning");
-        else setColor("btn btn-outline-warning");
-    }
     
+
+    function addFavorite(nombre){
+        if(color==="btn btn-outline-warning"){
+            actions.addFavorite(nombre);
+           // setHeart(<i className="fa-solid fa-heart"></i>);
+            setColor("btn btn-warning");
+        }else{
+            //setHeart(<i className="fa-regular fa-heart"></i>);
+            setColor("btn btn-outline-warning");
+        }
+        
+    }
+    //debemos conseguir modificar 1 solo corazon y no el de todas las fichas
+    //debemos conseguir guardar en store un [] con los datos de los favoritos
     return (
-	<div className="d-flex w-100 overflow-auto">
-        {store.planets.map((item)=>{return(
-        <div className="m-3" key={props.theid}>
-		<div className="card" style={{width: "18rem"}}>
-        <img src="https://picsum.photos/id/870/400/200?grayscale&blur=2" className="card-img-top" alt="..." />
+	<div className="d-flex">
+        <div className="m-3">
+		<div className="card border border-warning" style={{width: "18rem"}}>
+        <img src="https://picsum.photos/id/870/800/600?grayscale&blur=2" className="card-img-top" alt="..." />
         <div className="card-body">
-            <h5 className="card-title">{item.name}</h5>
-            <p className="card-text">Poblacion: {item.population}<br></br>Período Orbital: {item.orbital_period}</p>
+            <h5 className="card-title">{nombre}</h5>
+            <p className="card-text"></p>
             <div className="d-flex justify-content-between">
                 <Link to={`singleme/planet/${theid}`}>
                 <div className="p-0 bd-highlight">
@@ -31,13 +38,13 @@ export const CardPlanet = () =>{
                 </div>
                 </Link>
                 <div className="p-0 bd-highlight">
-                    <span href="#" className={color} onClick={addFavorite}>{heart}</span>
+                    <span href="#" className={color} onClick={()=>addFavorite(nombre)}><i className={`${actions.findFavorite(nombre)}`?"fa-regular fa-heart":"fa-solid fa-heart"}></i></span>
                 </div>
             </div>
-            
+            {/* `${actions.findFavorite(nombre)}`?"fa-regular fa-heart":"fa-solid fa-heart" */}
         </div>
         </div>	
-	    </div>)})}
+	    </div>
     </div>
 );
 };
--- src/js/views/sigleMe.js
+++ src/js/views/sigleMe.js
@@ -22,8 +22,8 @@ useEffect(()=>{
 		<>
         {type == "character" ? <Description name={store.dataSingle.name} label1="Name" label2="Birth Year" label3="Hair Color" label4="Eye Color" label5="Height" label6="Mass"
        data1={store.dataSingle.name} data2={store.dataSingle.birth_year} data3={store.dataSingle.hair_color} data4={store.dataSingle.eye_color} data5={store.dataSingle.height} data6={store.dataSingle.mass}/> 
-       : <Description name={store.dataSingle.name} label1="Name" label2="Birth Year" label3="Hair Color" label4="Eye Color" label5="Height" label6="Mass"
-       data1={store.dataSingle.name} data2={store.dataSingle.birth_year} data3={store.dataSingle.hair_color} data4={store.dataSingle.eye_color} data5={store.dataSingle.height} data6={store.dataSingle.mass}/>}
+       : <Description name={store.dataSingle.name} label1="Name" label2="Climate" label3="Diameter" label4="Gravity" label5="Population" label6="Terrain"
+       data1={store.dataSingle.name} data2={store.dataSingle.climate} data3={store.dataSingle.diameter} data4={store.dataSingle.gravity} data5={store.dataSingle.population} data6={store.dataSingle.terrain} />}
 		
 		</>
 	);}
